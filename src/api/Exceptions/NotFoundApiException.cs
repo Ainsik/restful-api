c88b38c31924d6b13e2baa74a00b89ed5f0b4f0a
Ainsik <pio.andrzejczuk@gmail.com>
2023-06-10 15:36:09 +0200

    add exceptions

---
+++ src/api/Exceptions/NotFoundApiException.cs
@@ -0,0 +1,15 @@
+﻿namespace api.Exceptions
+{
+    public class NotFoundApiException : ApiException
+    {
+        public NotFoundApiException() : base() { }
+
+        public NotFoundApiException(string message) : base(message)
+        {
+        }
+
+        public NotFoundApiException(string resource, string key) : base($"{resource} with key {key} not found.")
+        {
+        }
+    }
+}
