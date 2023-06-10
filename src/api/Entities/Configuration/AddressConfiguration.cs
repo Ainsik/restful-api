b02837223218a172be1b903915b14953a2b4e559
Ainsik <pio.andrzejczuk@gmail.com>
2023-06-10 14:51:40 +0200

    implement configuration for entities

---
+++ src/api/Entities/Configuration/AddressConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace api.Entities.Configuration
+{
+    public class AddressConfiguration : IEntityTypeConfiguration<Address>
+    {
+        public void Configure(EntityTypeBuilder<Address> builder)
+        {
+            builder.Property(a => a.City)
+                .IsRequired()
+                .HasMaxLength(30);
+
+            builder.Property(a => a.Street)
+                .IsRequired()
+                .HasMaxLength(30);
+
+            builder.Property(a => a.PostalCode)
+                .IsRequired()
+                .HasMaxLength(6)
+                .IsFixedLength();
+        }
+    }
+}
---
+++ src/api/Entities/Configuration/DoctorConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace api.Entities.Configuration
+{
+    public class DoctorConfiguration : IEntityTypeConfiguration<Doctor>
+    {
+        public void Configure(EntityTypeBuilder<Doctor> builder)
+        {
+            builder.HasOne(a => a.Address)
+                .WithOne(d => d.Doctor)
+                .HasForeignKey<Address>(d => d.DoctorId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Property(p => p.FirstName)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder.Property(p => p.LastName)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder.Property(p => p.Phone)
+                .IsRequired()
+                .HasMaxLength(9)
+                .IsFixedLength();
+
+            builder.Property(p => p.Specialization)
+                .IsRequired()
+                .HasMaxLength(50);
+        }
+    }
+}
