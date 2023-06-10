8129103d570eb7a0e8693f667b650db02a450b64
Ainsik <pio.andrzejczuk@gmail.com>
2023-06-10 15:27:18 +0200

    create dtos and validations

---
+++ src/api/DTOs/BaseDoctorDto.cs
@@ -0,0 +1,13 @@
+﻿namespace api.DTOs
+{
+    public class BaseDoctorDto
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Phone { get; set; }
+        public string Specialization { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string PostalCode { get; set; }
+    }
+}
---
+++ src/api/DTOs/CreateDoctorDto.cs
@@ -0,0 +1,10 @@
+﻿namespace api.DTOs
+{
+    public class CreateDoctorDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public string Specialization { get; set; }
+    }
+}
