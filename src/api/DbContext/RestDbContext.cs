b2c1f409ee740779cddfac1890d323a77c7a173b
Ainsik <pio.andrzejczuk@gmail.com>
2023-06-10 14:54:27 +0200

    add db context

---
+++ src/api/DbContext/RestDbContext.cs
@@ -0,0 +1,21 @@
+﻿using api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.DbContext
+{
+    public class RestDbContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        public RestDbContext(DbContextOptions<RestDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Doctor> Doctors { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+        }
+    }
+}
