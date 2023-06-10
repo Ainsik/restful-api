8d62bf96ae8d27712bd293d1e359ea2d21e90b1c
Ainsik <pio.andrzejczuk@gmail.com>
2023-06-10 14:50:23 +0200

    create entities

---
+++ src/api/Entities/Address.cs
@@ -0,0 +1,12 @@
+﻿namespace api.Entities
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string PostalCode { get; set; }
+        public int DoctorId { get; set; }
+        public virtual Doctor Doctor { get; set; }
+    }
+}
---
+++ src/api/Entities/Doctor.cs
@@ -0,0 +1,12 @@
+﻿namespace api.Entities
+{
+    public class Doctor
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public string Specialization { get; set; }
+        public virtual Address Address { get; set; }
+    }
+}
