8f26eb60019810c95588456c02388cb7b4f5c914
Ainsik <pio.andrzejczuk@gmail.com>
2023-06-11 19:35:41 +0200

    implement get all doctors

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,45 @@
 <template>
-	<section></section>
+	<section>
+		<div>
+			<h2>GET ALL</h2>
+			<button @click="getAllDoctors">AllDoctors</button>
+			<div v-if="isValid">
+				<ul v-for="doctor in doctors" :key="doctor.id">
+					<li>
+						ID: {{ doctor.id }} <br />
+						FullName: {{ doctor.fullName }}
+					</li>
+				</ul>
+			</div>
+		</div>
+	</section>
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+const API = "https://localhost:44389/api/doctors";
+
+export default {
+	data() {
+		return {
+			isValid: true,
+			doctors: [],
+		};
+	},
+	methods: {
+		async getAllDoctors() {
+			this.isValid = true;
+			try {
+				await axios.get(API).then((result) => {
+					const { data } = result;
+					this.doctors = data;
+				});
+			} catch (error) {
+				this.isValid = false;
+			}
+		},
+	},
+};
 </script>
 
 <style>
